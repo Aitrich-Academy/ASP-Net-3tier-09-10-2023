b57fa450aa152506d9e9199a88b729cfaf50cb99
ashilymariya <ashilymariyapv@gmail.com>
2023-10-13 16:14:46 +0530

    category add

---
+++ MiniProject/BLL/Admin.cs
@@ -0,0 +1,101 @@
+﻿using DAL;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Admin
+    {
+        private DataBase dataBase = new DataBase();
+        public Property property = new Property();
+        private SortedList lists = new SortedList();
+
+        public string Category_Insert()
+        {
+            lists.Clear();
+            lists.Add("Category_Id", property.Category_Id);
+            lists.Add("Category_Name",property.Category_Name);
+
+            return dataBase.ExecuteProcedure(lists, "Category_Insert");
+        }
+        public string Dish_Insert()
+        {
+            lists.Clear();
+            lists.Add("Dishe_Name", property.Dish_Name);
+            lists.Add("Price", property.Price);
+            lists.Add("Image", property.Image);
+            lists.Add("Category_Id", property.Category_Id);
+
+            return dataBase.ExecuteProcedure(lists,"Dish_Insert");
+
+        }
+
+        //public string Dish_Update()
+        //{
+        //    S1.Clear();
+        //    S1.Add("Dish_Id", property.Dish_Id);
+        //    S1.Add("Dishe_Name", property.Dish_Name);
+        //    S1.Add("Price", property.Price);
+        //    S1.Add("IMAGE", property.Image);
+        //    S1.Add("Category_Id", property.Category_Id);
+        //    return dataBase.ExecuteProcedure(S1, "Dish_Update ");
+        //}
+
+        //public string Dish_Delete()
+        //{
+        //    S1.Clear();
+        //    S1.Add("DishID", property.Dish_Id);
+        //    return dataBase.ExecuteProcedure(S1, "Dish_Delete");
+        //}
+        public List<Property> SelectAllDishes()
+        {
+            DataTable dt = new DataTable();
+            dt = dataBase.GetDataTable("GetCategoriesAndDishes");
+            List<Property> list = new List<Property>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                list.Add(new Property
+                {
+
+                    //Category_Name = dr[" Category_Name"].ToString(),
+                    Dish_Id = Convert.ToInt32(dr["DishID"]),
+                    Dish_Name = dr["Dishe_Name"].ToString(),
+                    Price = Convert.ToDecimal(dr["Price"]),
+                    Image = dr["Image"].ToString(),
+                    Category_Id = Convert.ToInt32(dr["Category_id"])
+
+
+                });
+            }
+            return list;
+        }
+        public List<Property> SelectAllCategory()
+        {
+            DataTable dt = new DataTable();
+            dt = dataBase.GetDataTable("Category_Select");
+            List<Property> lists = new List<Property>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                lists.Add(new Property
+                {
+
+                    Category_Name = dr["Category_Name"].ToString(),
+                    //Dish_Id = Convert.ToInt32(dr["DishID"]),
+                    //Dish_Name = dr["Dishe_Name"].ToString(),
+                    //Price = Convert.ToDecimal(dr["Price"]),
+                    //Image = dr["Image"].ToString(),
+                    Category_Id = Convert.ToInt32(dr["CategoryID"])
+
+
+                });
+            }
+            return lists;
+        }
+
+    }
+}
--- MiniProject/MiniProject/CategoryManagement.aspx.cs
+++ MiniProject/MiniProject/CategoryManagement.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,113 @@ namespace MiniProject
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        Admin admin = new Admin();
+     
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            GridData();
+            map1.Visible = false;
+        }
+        public void InsertCategory()
+        {
+            if (int.TryParse(CategoryId.Text.Trim(), out int categoryId))
+            {
+                admin.property.Category_Id = categoryId;
+                admin.property.Category_Name = DropDownList1.SelectedItem.Text;
+                string result = admin.Category_Insert();
+                if (result == "Success")
+                {
+                    lblmsg.Text = "Insertion was successful.";
+                    lblmsg.Visible = true;
+                }
+                else
+                {
+                    lblmsg.Visible = true;
+                    lblmsg.Text = "Error: " + result; // Show the specific error message.
+                }
+            }
+            else
+            {
+                lblmsg.Visible = true;
+                lblmsg.Text = "Error: Invalid Category ID"; // Handle invalid CategoryId input.
+            }
+            GridData();
+        }
+
+        protected void Btncategory_Click(object sender, EventArgs e)
+        {
+            InsertCategory();
+        }
+
+        
+        private string GetRandomText()
+        {
+            string randomtext = "";
+            string alpahbets = "0123456789";
+
+            Random r = new Random();
+            for (int j = 0; j <= 3; j++)
+            {
+                randomtext += alpahbets[r.Next(alpahbets.Length)];
+
+            }
+            return randomtext.ToString();
+        }
+        public void InsertDish()
+        {
+            if (FileUpload1.HasFile == true)
+            {
+                string filename = GetRandomText();
+                string path = Server.MapPath("~/Picture/");
+                FileUpload1.SaveAs(path + filename + ".jpg");
+
+                admin.property.Image=("~/Picture/" + filename + ".jpg");
+                admin.property.Category_Id = int.Parse(category.Text.Trim());
+                admin.property.Dish_Name = Dishname.Text.Trim().ToString();
+                admin.property.Price = decimal.Parse(price.Text.Trim());
+                string result = admin.Dish_Insert();
+                category.Text = "";
+                CategoryId.Text = "";
+                Dishname.Text = "";
+                price.Text = "";
+                DropDownList1.SelectedIndex = 0;
+                Hiddenval.Value = "-1";
+                if (result == "SUCCESS")
+                {
+                    lblmsg.Visible = true;
+                    lblmsg.Text = "Inserted Successfully";
+
+                }
+                else
+                {
+                    lblmsg.Visible = true;
+                    lblmsg.Text = "Error";
+                }
+                //GridData();
+            }
+        }
+        protected void submitbtn_Click(object sender, EventArgs e)
+        {
+            if (Hiddenval.Value == "-1")
+            {
+                InsertDish();
+            }
+            //else
+            //{
+
+            //}
+
+                
+        }
+
+
+        public void GridData()
+        {
+            GridView2.DataSource = admin.SelectAllCategory();
+            GridView2.DataBind();
 
         }
+
     }
 }
