12377a9b3e6e535f495276aafa590efe9948134d
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2023-11-01 16:34:14 +0530

    MenuList

--- MiniProject/MiniProject/MenuList.aspx.designer.cs
+++ MiniProject/MiniProject/MenuList.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace MiniProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
